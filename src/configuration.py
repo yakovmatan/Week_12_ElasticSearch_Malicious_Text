a886baed3613513a008105f99bfe0ca9353d0d88
yakov matan <yakovmatan@gmail.com>
2025-09-01 10:14:50 +0300

    create connection to elastic

---
+++ src/configuration.py
@@ -0,0 +1,24 @@
+import os
+import time
+from elasticsearch import Elasticsearch
+
+
+class Configuration:
+
+    def __init__(self):
+        conn = os.getenv('ES_HOST')
+        self.es:Elasticsearch = Elasticsearch(f'http://{conn}:9200')
+        self.__availability_check()
+
+    def get_conn(self) -> Elasticsearch :
+        return self.es
+
+    def __availability_check(self):
+        while True:
+            try:
+                if self.es.ping():
+                    return "Elasticsearch is ready"
+
+            except ConnectionError:
+                pass
+            time.sleep(2)
